4a01251a812cc079df8f814d5f944ebee58f686f
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2024-07-07 09:39:44 +0200

    Add files via upload

---
+++ seer.js
@@ -0,0 +1,443 @@
+//dialog function
+$(function() {
+            $( "#dialog-7" ).dialog({
+               autoOpen: true, 
+               resize: function( event, ui ) {
+                  $( this ).dialog( "option", "title",
+	         ui.size.height +- " x " +- ui.size.width );
+               }
+            });
+            $( "#opener-6" ).click(function() {
+               $( "#dialog-7" ).dialog( "open" );
+            });
+         });
+//end dialog function    
+
+//dialog function
+$(function() {
+            $( "#dialog-8" ).dialog({
+               autoOpen: false, 
+               resize: function( event, ui ) {
+                  $( this ).dialog( "option", "title",
+	         ui.size.height +- " x " +- ui.size.width );
+               }
+            });
+            $( "#opener-7" ).click(function() {
+               $( "#dialog-8" ).dialog( "open" );
+            });
+         });
+//end dialog function    
+
+$(document).ready (function(){
+$("img").attr("loading", "lazy");
+});
+
+/*function to change button behavior*/
+function salmanda1(){
+document.getElementById ("demo1"). style. color ="white";
+
+document.getElementById ("demo1"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo1"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo1"). style. background ="maroon";
+}
+
+function salmanda2(){
+document.getElementById ("demo2"). style. color ="white";
+
+document.getElementById ("demo2"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo2"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo2"). style. background ="maroon";
+}
+
+function salmanda3(){
+document.getElementById ("demo3"). style. color ="white";
+
+document.getElementById ("demo3"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo3"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo3"). style. background ="maroon";
+}
+
+function salmanda4(){
+document.getElementById ("demo4"). style. color ="white";
+
+document.getElementById ("demo4"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo4"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo4"). style. background ="maroon";
+}
+
+function salmanda5(){
+document.getElementById ("demo5"). style. color ="white";
+
+document.getElementById ("demo5"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo5"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo5"). style. background ="maroon";
+}
+
+/*function to change button behavior horoscope signs*/
+
+function salmandai(){
+document.getElementById ("demoi"). style. color ="white";
+
+document.getElementById ("demoi"). style. fontweight ="bold";
+
+document.getElementById
+ ("demoi"). style. fontsize ="30%";
+
+document.getElementById
+ ("demoi"). style. background ="maroon";
+}
+
+function salmandaii(){
+document.getElementById ("demoii"). style. color ="white";
+
+document.getElementById ("demoii"). style. fontweight ="bold";
+
+document.getElementById
+ ("demoii"). style. fontsize ="30%";
+
+document.getElementById
+ ("demoii"). style. background ="maroon";
+}
+
+function salmandaiii(){
+document.getElementById ("demoiii"). style. color ="white";
+
+document.getElementById ("demoiii"). style. fontweight ="bold";
+
+document.getElementById
+ ("demoiii"). style. fontsize ="30%";
+
+document.getElementById
+ ("demoiii"). style. background ="maroon";
+}
+
+function salmandaiv(){
+document.getElementById ("demoiv"). style. color ="white";
+
+document.getElementById ("demoiv"). style. fontweight ="bold";
+
+document.getElementById
+ ("demoiv"). style. fontsize ="30%";
+
+document.getElementById
+ ("demoiv"). style. background ="maroon";
+}
+
+function salmandav(){
+document.getElementById ("demov"). style. color ="white";
+
+document.getElementById ("demov"). style. fontweight ="bold";
+
+document.getElementById
+ ("demov"). style. fontsize ="30%";
+
+document.getElementById
+ ("demov"). style. background ="maroon";
+}
+
+function salmandavi(){
+document.getElementById ("demovi"). style. color ="white";
+
+document.getElementById ("demovi"). style. fontweight ="bold";
+
+document.getElementById
+ ("demovi"). style. fontsize ="30%";
+
+document.getElementById
+ ("demovi"). style. background ="maroon";
+}
+
+function salmandavii(){
+document.getElementById ("demovii"). style. color ="white";
+
+document.getElementById ("demovii"). style. fontweight ="bold";
+
+document.getElementById
+ ("demovii"). style. fontsize ="30%";
+
+document.getElementById
+ ("demovii"). style. background ="maroon";
+}
+
+function salmandaviii(){
+document.getElementById ("demoviii"). style. color ="white";
+
+document.getElementById ("demoviii"). style. fontweight ="bold";
+
+document.getElementById
+ ("demoviii"). style. fontsize ="30%";
+
+document.getElementById
+ ("demoviii"). style. background ="maroon";
+}
+
+function salmandaix(){
+document.getElementById ("demoix"). style. color ="white";
+
+document.getElementById ("demoix"). style. fontweight ="bold";
+
+document.getElementById
+ ("demoix"). style. fontsize ="30%";
+
+document.getElementById
+ ("demoix"). style. background ="maroon";
+}
+
+function salmandax(){
+document.getElementById ("demox"). style. color ="white";
+
+document.getElementById ("demox"). style. fontweight ="bold";
+
+document.getElementById
+ ("demox"). style. fontsize ="30%";
+
+document.getElementById
+ ("demox"). style. background ="maroon";
+}
+
+function salmandaxi(){
+document.getElementById ("demoxi"). style. color ="white";
+
+document.getElementById ("demoxi"). style. fontweight ="bold";
+
+document.getElementById
+ ("demoxi"). style. fontsize ="30%";
+
+document.getElementById
+ ("demoxi"). style. background ="maroon";
+}
+
+function salmandaxii(){
+document.getElementById ("demoxii"). style. color ="white";
+
+document.getElementById ("demoxii"). style. fontweight ="bold";
+
+document.getElementById
+ ("demoxii"). style. fontsize ="30%";
+
+document.getElementById
+ ("demoxii"). style. background ="maroon";
+}
+
+/*function to change button behavior menu*/
+
+function salmanda6(){
+document.getElementById ("demo6"). style. color ="white";
+
+document.getElementById ("demo6"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo6"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo6"). style. background ="maroon";
+}
+
+function salmanda7(){
+document.getElementById ("demo7"). style. color ="white";
+
+document.getElementById ("demo7"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo7"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo7"). style. background ="maroon";
+}
+
+function salmanda8(){
+document.getElementById ("demo8"). style. color ="white";
+
+document.getElementById ("demo8"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo8"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo8"). style. background ="maroon";
+}
+
+function salmanda9(){
+document.getElementById ("demo9"). style. color ="white";
+
+document.getElementById ("demo9"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo9"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo9"). style. background ="maroon";
+}
+
+function salmanda10(){
+document.getElementById ("demo10"). style. color ="white";
+
+document.getElementById ("demo10"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo10"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo10"). style. background ="maroon";
+}
+
+function salmanda11(){
+document.getElementById ("demo11"). style. color ="white";
+
+document.getElementById ("demo11"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo11"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo11"). style. background ="maroon";
+}
+
+function salmanda12(){
+document.getElementById ("demo12"). style. color ="white";
+
+document.getElementById ("demo12"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo12"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo12"). style. background ="maroon";
+}
+
+function salmanda13(){
+document.getElementById ("demo13"). style. color ="white";
+
+document.getElementById ("demo13"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo13"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo13"). style. background ="maroon";
+}
+
+function salmanda14(){
+document.getElementById ("demo14"). style. color ="white";
+
+document.getElementById ("demo14"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo14"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo14"). style. background ="maroon";
+}
+
+function salmanda15(){
+document.getElementById ("demo15"). style. color ="white";
+
+document.getElementById ("demo15"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo15"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo15"). style. background ="maroon";
+}
+
+function salmanda16(){
+document.getElementById ("demo16"). style. color ="white";
+
+document.getElementById ("demo16"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo16"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo16"). style. background ="maroon";
+}
+
+//function to open portal 
+function africaOnline(){
+open("https://salmandascript.github.io/AfricaOnline/index.html");
+}
+
+//function to open help
+function help(){
+open("https://salmandascript.github.io/AboutSalmanda/help.html");
+}
+
+//function to open contacts
+function contact(){
+open("https://salmandascript.github.io/AboutSalmanda/contact.html");
+}
+
+//function to open Terms of service 
+function Service(){
+open("https://salmandascript.github.io/AboutSalmanda/Service.html");
+}
+
+//function to open About
+function about(){
+open("https://salmandascript.github.io/AboutSalmanda/about.html");
+}
+
+//function to open My President programme 
+function myPresident(){
+open("https://salmandascript.github.io/MyPresident2/index.html");
+}
+
+//function to open Capital Cities
+function capitalCities(){
+open("https://salmandascript.github.io/CapitalCities.App/index.html");
+}
+
+//function to open Sm Technology
+function SmTech(){
+open("https://salmandascript.github.io/SM Technology/smtech.html");
+}
+
+
+function linkWikipedia(){
+open("https://salmandascript.github.io/LinkWikipedia/");
+}
+
+function seer(){
+open("https://salmandascript.github.io/Seer/");
+}
+
+function royalFM(){
+open("https://salmandascript.github.io/RoyalFM/");
+}
+
+function programming(){
+open("https://salmandascript.github.io/SoftwareService/");
+}
+
+function salmandaMusiq(){
+open("https://salmandascript.github.io/Salmandamusiq/");
+}
+
+function newsChannel(){
+open("https://salmandascript.github.io/NewsChannel/");
+}
+
+function movieChannel(){
+open("https://salmandascript.github.io/MovieChannel/");
+}
